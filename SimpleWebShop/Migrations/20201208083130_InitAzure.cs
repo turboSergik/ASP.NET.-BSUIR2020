435e7aeb60c4d5c1e7819f0d1afb66c34eb592f9
Piskun-Sergey-Nyblecraft <sergey.p@nyblecraft.com>
2020-12-12 12:16:22 +0300

    Updated config for web deploy

---
+++ SimpleWebShop/Migrations/20201208083130_InitAzure.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleWebShop.Migrations
+{
+    public partial class InitAzure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- SimpleWebShop/Startup.cs
+++ SimpleWebShop/Startup.cs
@@ -17,6 +17,7 @@ namespace SimpleWebShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
 
         private IConfigurationRoot _confString;
 
